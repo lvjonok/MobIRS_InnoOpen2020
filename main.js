d1de737fcc06702a7933daecc8544ddd3e2bb987
lvjonok <42120385+lvjonok@users.noreply.github.com>
2020-05-03 10:27:13 +0300

    added logic to turn

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ Robot = function(leftMotor, rightMotor, leftLineSensor, rightLineSensor, leftSid
 		
 		var eL = this.encoderLeft.read();
 		var eR = this.encoderRight.read();
-		while (eL - seL < enc || eR - seR < enc){
+		while ((tL > eL && tR > eR && enc > 0)||(tL < eL && tR < eR && enc < 0)){ // (eL - seL < enc || eR - seR < enc){
 			var eL = this.encoderLeft.read();
 			var eR = this.encoderRight.read();
 			this.setSpeed(speed, speed);
@@ -52,8 +52,8 @@ Robot = function(leftMotor, rightMotor, leftLineSensor, rightLineSensor, leftSid
 	this.turnDegrees = function(degrees){
 		var const_enc = this.sm2cpr(this.track_sm * Math.PI * (degrees / 360));
 		var direction = this.sign(const_enc);
-		print(const_enc);
-		print(direction);
+		// print(const_enc);
+		// print(direction);
 		this.target_encoders['left'] += const_enc;
 		this.target_encoders['right'] -= const_enc;
 
@@ -72,7 +72,42 @@ Robot = function(leftMotor, rightMotor, leftLineSensor, rightLineSensor, leftSid
 			this.setSpeed((this.target_encoders['left'] - eL) * 1.4, (this.target_encoders['right'] - eR) * 1.4);
 		}
 		this.setSpeed(0, 0);
+	};
+	this.turnDegreesOneWheel = function(degrees){
+		var const_enc = this.sm2cpr(this.track_sm * Math.PI * (degrees / 180));
+		var direction = this.sign(const_enc);
+		// print(const_enc);
+		// print(direction);
 
+		const_enc = abs(const_enc);
+
+		if (direction == 1){
+			this.target_encoders['left'] += const_enc;
+		} else {
+			this.target_encoders['right'] += const_enc;
+		}
+		
+		var eL = this.encoderLeft.read();
+		var eR = this.encoderRight.read();
+		
+		while ((this.target_encoders['left'] > eL && direction == 1) || (this.target_encoders['right'] > eR && direction == -1)){
+			eL = this.encoderLeft.read();
+			eR = this.encoderRight.read();
+			if (direction == 1){
+				this.setSpeed(50, 0);
+			} else {
+				this.setSpeed(0, 50);
+			}
+		}
+		// this.setSpeed(0, 0);
+		// wait(3000);
+		var st = Date.now();
+		while (Date.now() - st < 500){
+			var eL = this.encoderLeft.read();
+			var eR = this.encoderRight.read();
+			this.setSpeed((this.target_encoders['left'] - eL) * 10, (this.target_encoders['right'] - eR) * 10);
+		}
+		this.setSpeed(0, 0);
 	}
 	this.setSpeed = function(lS, rS){
 		if (rS == undefined) rS = lS;
@@ -116,14 +151,12 @@ Robot = function(leftMotor, rightMotor, leftLineSensor, rightLineSensor, leftSid
 		// var const_enc = 750; // encoders to reach next cell
 		var const_enc = (52.5 * (2 / 3) / (this.wheelD_sm * Math.PI) * 360);
 		var speed = 90;
-		var map = [1, undefined, undefined, undefined]; // contains available movements in this cell, direction is regarding moving
+		var map = [undefined, undefined, 1, undefined]; // contains available movements in this cell, direction is regarding moving
 		/*
 			map descriptor:
 				1 - way available
 				-1 - no way
 		*/
-		// var seL = this.encoderLeft.read();
-		// var seR = this.encoderRight.read();
 		var teL = this.target_encoders['left'] + const_enc;
 		var teR = this.target_encoders['right'] + const_enc;
 
@@ -133,26 +166,48 @@ Robot = function(leftMotor, rightMotor, leftLineSensor, rightLineSensor, leftSid
 		var surface_color = [undefined, undefined, undefined, undefined, undefined];
 		var surface_history = [[],[],[],[],[]];
 
-		
-
 		var ll = const_enc - this.target_encoders['left'] + this.encoderLeft.read(); //  this.encoderLeft.read() - seL;
 		var lr = const_enc - this.target_encoders['right'] + this.encoderRight.read();
 
 		for (var i = 0; i < 5; i++){
 			var sens = this.sensors[i]()
 			surface_color[i] = sens < 50 ? 1 : -1;
-			surface_history[i].push([surface_color[i], ll]);
+			surface_history[i].push([surface_color[i], Math.round(ll)]);
 		}
+
+		var flr = false; // true - if we are passing round turn
+
+		var output = {'direction' : 0, 'map' : map, 'movement' : undefined};
 
 		while (ll < const_enc || lr < const_enc){
 			for (var i = 0; i < 5; i++){
 				var sens = this.sensors[i]();
-				if (sens < 50 && surface_color[i] == -1){
-					surface_color[i] = 1;
-					surface_history[i].push([surface_color[i], ll]);
-				} else if (sens > 50 && surface_color[i] == 1) {
-					surface_color[i] = -1;
-					surface_history[i].push([surface_color[i], ll]);
+				if ((sens < 50 && surface_color[i] == -1) || (sens > 50 && surface_color[i] == 1)){
+					surface_color[i] *= -1;
+					surface_history[i].push([surface_color[i], Math.round(ll)]);
+					if (i == 2 && ll > 120 && ll < 260) flr = true;
+					else if (i != 2 && flr == true){
+						print(surface_history[2]);
+						this.target_encoders['left'] = teL - 170;
+						this.target_encoders['right'] = teR - 170; // this.encoderRight.read();
+						var st = Date.now();
+						while (Date.now() - st < 1000){
+							var eL = this.encoderLeft.read();
+							var eR = this.encoderRight.read();
+							this.setSpeed((this.target_encoders['left'] - eL) * 8, (this.target_encoders['right'] - eR) * 8);
+						}
+						this.setSpeed(0, 0);
+						// robot.moveEncoders(305, 50);
+						if (i == 1){
+							output['map'][3] = 1;
+							robot.turnDegreesOneWheel(-90);
+						} else if (i == 3){
+							output['map'][1] = 1;
+							robot.turnDegreesOneWheel(90);
+						}						
+						robot.moveEncoders(-184, -30);
+						return output;
+					}
 				}
 			}
 
@@ -160,7 +215,7 @@ Robot = function(leftMotor, rightMotor, leftLineSensor, rightLineSensor, leftSid
 			lr = const_enc - this.target_encoders['right'] + this.encoderRight.read();
 			this.setSpeed(speed - (ll - lr) * 2, speed + (ll - lr) * 2);
 		}
-		print(surface_history);
+		print(surface_history[2]);
 		var st = Date.now();
 		while (Date.now() - st < 250){
 			var eL = this.encoderLeft.read();
@@ -168,6 +223,7 @@ Robot = function(leftMotor, rightMotor, leftLineSensor, rightLineSensor, leftSid
 			this.setSpeed((teL - eL) * 3, (teR - eR) * 3);
 		}
 		this.setSpeed(0, 0);
+		return {};
 	};
 };
 
@@ -180,6 +236,22 @@ var main = function(){
 	robot = new Robot("M4", "M3", "A1", "A2", "A3", "A4", "A5");
 
 	// robot.turnDegrees(90);
+	robot.driveSector();
+	robot.turnDegrees(90);
+	robot.driveSector();
+	robot.turnDegrees(-90);
+	robot.driveSector();
+	robot.driveSector();
+	robot.driveSector();
+	robot.turnDegrees(-90);
+	robot.driveSector();
+	robot.driveSector();
+	robot.driveSector();
+	robot.driveSector();
+	robot.driveSector();
+	robot.turnDegrees(90);
+	robot.driveSector();
+	robot.turnDegrees(-90);
 	robot.driveSector();
 }
 
